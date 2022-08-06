3fe4cd3f645aec1b3fbe413b69530caea1ddcf8a
DeGiorgio Nicolas <nicolasdegiorgio@gmail.com>
2022-08-06 10:12:16 -0300

    Versión 0.1: Apps creadas

---
+++ src/productos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class ProductosDetailing (models.Model):
+    nombre = models.CharField(max_length=150)
+    marca = models.CharField(max_length=150)
+    precio = models.FloatField()
+    tipo = models.DateField(max_length=10) #Todavia no se le da mucha utilidad
+    stock = models.IntegerField(default=50)
+    
+    #Defino el str para ver en el panel de administración
+    def __str__(self) -> str:
+        return f'Nombre: {self.nombre} - Precio: ${self.precio}'
+    
+
+class ProductosLimpieza(models.Model):
+    
+    nombre = models.CharField(max_length=150)
+    marca = models.CharField(max_length=150)
+    precio = models.FloatField()
+    tipo = models.DateField(max_length=10) #Todavia no se le da mucha utilidad
+    stock = models.IntegerField(default=50)
+    
+    #Defino el str para ver en el panel de administración
+    def __str__(self) -> str:
+        return f'Nombre: {self.nombre} - Precio: ${self.precio}'
